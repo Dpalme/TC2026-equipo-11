40f52ede867c4b364126b24866a8bbf3df55de68
Dpalme <diegoPalmerin24@gmail.com>
2021-09-30 17:23:48 -0500

    json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.sendData("NAHHHHHHHHHHHHHH CREOOOOOOOOOOOO");
+    return res.json("NAHHHHHHHHHHHHHH CREOOOOOOOOOOOO");
 })
 
 const listener = app.listen(8083, () => { console.log(listener.address().port); })
