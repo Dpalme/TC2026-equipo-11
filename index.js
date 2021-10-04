ffae8cde02760498ee4c133ebe0ba109b6b3fb6d
Diego Palmerin <57266493+Dpalme@users.noreply.github.com>
2021-10-04 16:36:37 -0500

    load routes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require('path')
 const app = express();
+const routes = require('./routes/videojuegos.js');
 
 const sequelize = require('./utils/database')
 app.use(express.static(path.join(__dirname,'public')))
-
+app.use(routes);
 
 app.get('/', (req, res) => {
     return res.json("NAHHHHHHHHHHHHHH CREOOOOOOOOOOOO");
