f4dac9614eeb0a4118b86ed84f8fd14d92cc2cb7
ZoeCD <>
2021-10-04 16:32:00 -0500

    cambios index

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const path = require('path')
 const app = express();
+
+const sequelize = require('./utils/database')
+app.use(express.static(path.join(__dirname,'public')))
+
 
 app.get('/', (req, res) => {
     return res.json("NAHHHHHHHHHHHHHH CREOOOOOOOOOOOO");
