dcc8ace0a4a82345e28978a9f95e3daca6a247da
Diego Palmerin <57266493+Dpalme@users.noreply.github.com>
2021-10-04 16:40:02 -0500

    Update videojuegos.js

--- routes/videojuegos.js
+++ routes/videojuegos.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const vjController = require('../controllers/videojuegos')
+const vjController = require('../controllers/videojuegos.js')
 const router = express.Router()
 
 router.post('/agregarVideojuego',vjController.postAgregarVideojuego)
